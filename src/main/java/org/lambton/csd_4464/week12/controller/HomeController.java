ef66a4302999d9634504db91fe5b0de7569e0162
Fernando L. <aleguia88@hotmail.com>
2025-03-24 09:44:44 -0400

    initial project

--- src/main/java/org/lambton/csd_4464/week12/controller/HomeController.java
+++ src/main/java/org/lambton/csd_4464/week12/controller/HomeController.java
@@ -1,4 +1,17 @@
 package org.lambton.csd_4464.week12.controller;
 
+import org.lambton.csd_4464.week12.model.SampleModel;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        SampleModel sample = new SampleModel(1L, "Hello from Lombok!");
+        model.addAttribute("sample", sample);
+        return "index";
+    }
 }
--- src/main/java/org/lambton/csd_4464/week12/model/SampleModel.java
+++ src/main/java/org/lambton/csd_4464/week12/model/SampleModel.java
@@ -1,4 +1,13 @@
 package org.lambton.csd_4464.week12.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class SampleModel {
+    private Long id;
+    private String message;
 }
